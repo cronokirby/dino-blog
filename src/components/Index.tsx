514b172ff1dc4d6eb46673db3f9f7c1334d5d60d
Lucas Meier <lucas@cronokirby.com>
2025-05-05 21:34:31 -0700

    Use PT family of fonts

--- src/components/Index.tsx
+++ src/components/Index.tsx
@@ -4,10 +4,16 @@ export default () => {
       <head>
         <title>Dino Blog!!!</title>
       </head>
-      <body>
-        <h1 className="text-red-600 text-4xl">Heading 1</h1>
-        <p>This should be statically rendered.</p>
-        <h1>Client Component:</h1>
+      <body className="bg-slate-100 font-serif text-slate-900">
+        <h1 className="text-4xl">About</h1>
+        <p>Here's an example of a large body of text.</p>
+        <pre className="font-mono">
+          <code>
+            main :: IO ()
+            <br />
+            main = putStrLn "Hello World!"
+          </code>
+        </pre>
       </body>
     </html>
   );
